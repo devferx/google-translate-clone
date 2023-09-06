1a31061bff8716d157af67a63168efddbf087ef9
devferx <ferkki@hotmail.com>
2023-09-06 10:46:43 -0400

    Add reducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,67 @@
+import { useReducer } from 'react'
+
+import type { State, ActionTypes } from './types'
+
 import './App.css'
 
+const initialState: State = {
+  fromLanguage: 'auto',
+  toLanguage: 'en',
+  fromText: '',
+  result: '',
+  isLoading: false
+}
+
+function reducer(state: State, action: ActionTypes): State {
+  switch (action.type) {
+    case 'INTERCHANGE_LANGUAGES':
+      return {
+        ...state,
+        fromLanguage: state.toLanguage,
+        toLanguage: state.fromLanguage
+      }
+    case 'SET_FROM_LANGUAGE':
+      return {
+        ...state,
+        fromLanguage: action.payload
+      }
+    case 'SET_TO_LANGUAGE':
+      return {
+        ...state,
+        toLanguage: action.payload
+      }
+    case 'SET_FROM_TEXT':
+      return {
+        ...state,
+        isLoading: true,
+        fromText: action.payload,
+        result: ''
+      }
+    case 'SET_RESULT':
+      return {
+        ...state,
+        isLoading: false,
+        result: action.payload
+      }
+    default:
+      return state
+  }
+}
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState)
+  const { fromLanguage, fromText, isLoading, result, toLanguage } = state
+
   return (
     <div className='App'>
       <h1>Google Translate</h1>
+      <button
+        onClick={() => {
+          dispatch({ type: 'SET_FROM_LANGUAGE', payload: 'es' })
+        }}
+      >
+        Cambiar a Español
+      </button>
     </div>
   )
 }
---
+++ src/types.ts
@@ -0,0 +1,16 @@
+export interface State {
+  fromLanguage: string
+  toLanguage: string
+  fromText: string
+  result: string
+  isLoading: boolean
+}
+
+export type ActionTypes =
+  | {
+      type: 'INTERCHANGE_LANGUAGES'
+    }
+  | { type: 'SET_FROM_LANGUAGE'; payload: string }
+  | { type: 'SET_TO_LANGUAGE'; payload: string }
+  | { type: 'SET_FROM_TEXT'; payload: string }
+  | { type: 'SET_RESULT'; payload: string }
