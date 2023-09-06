14b2ce123b7e39797212f9887248c09a9bcfbdb2
devferx <ferkki@hotmail.com>
2023-09-06 10:52:55 -0400

    Update reducer to useStore hook

---
+++ src/hooks/useStore.ts
@@ -0,0 +1,74 @@
+import { useReducer } from 'react'
+import type { State, ActionTypes } from './useStore.types'
+
+const initialState: State = {
+  fromLanguage: 'auto',
+  toLanguage: 'en',
+  fromText: '',
+  result: '',
+  isLoading: false
+}
+
+function reducer(state: State, action: ActionTypes): State {
+  switch (action.type) {
+    case 'INTERCHANGE_LANGUAGES':
+      return {
+        ...state,
+        fromLanguage: state.toLanguage,
+        toLanguage: state.fromLanguage
+      }
+    case 'SET_FROM_LANGUAGE':
+      return {
+        ...state,
+        fromLanguage: action.payload
+      }
+    case 'SET_TO_LANGUAGE':
+      return {
+        ...state,
+        toLanguage: action.payload
+      }
+    case 'SET_FROM_TEXT':
+      return {
+        ...state,
+        isLoading: true,
+        fromText: action.payload,
+        result: ''
+      }
+    case 'SET_RESULT':
+      return {
+        ...state,
+        isLoading: false,
+        result: action.payload
+      }
+    default:
+      return state
+  }
+}
+
+export function useStore() {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  const intercahngeLanguages = () => {
+    dispatch({ type: 'INTERCHANGE_LANGUAGES' })
+  }
+
+  const setFromLanguage = (payload: string) => {
+    dispatch({ type: 'SET_FROM_LANGUAGE', payload })
+  }
+
+  const setToLanguage = (payload: string) => {
+    dispatch({ type: 'SET_TO_LANGUAGE', payload })
+  }
+
+  const setFromText = (payload: string) => {
+    dispatch({ type: 'SET_FROM_TEXT', payload })
+  }
+
+  return {
+    ...state,
+    intercahngeLanguages,
+    setFromLanguage,
+    setToLanguage,
+    setFromText
+  }
+}
